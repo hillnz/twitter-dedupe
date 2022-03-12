2977ff563d5995566aa348a8092abeb614a3d6c9
Jono Hill <jono@hillnz.com>
2022-03-13 12:36:49 +1300

    Support dynamodb for cache and ability to run once

--- twitterdedupe/caches.py
+++ twitterdedupe/caches.py
@@ -1,13 +1,18 @@
 """
 caches
-From http://blog.seevl.fm/2013/11/22/simple-caching-with-redis/
 """
 
+from xml.dom import NotSupportedErr
+import boto3
+import json
 import pickle
+import time
 
 
 class RedisCache(object):
     """Redis cache"""
+
+    # From http://blog.seevl.fm/2013/11/22/simple-caching-with-redis/
 
     def __init__(self, redis, prefix='cache|', timeout=60*60):
         """Create Redis cache instance using a StrictRedis connection,
@@ -34,3 +39,51 @@ class RedisCache(object):
         or only keys matching an optional pattern"""
         keys = self._redis.keys(self._prefix + pattern + '*')
         [self._redis.delete(*keys[i:i+step]) for i in range(0, len(keys), step)]
+
+
+class DynamoCache:
+    """AWS DynamoDB cache"""
+    
+
+    def __init__(self, table_name, prefix='cache|', timeout=60*60):
+        """Create DynamoDB cache instance"""
+        assert table_name
+        self._table = boto3.resource('dynamodb').Table(table_name)
+        self._prefix = prefix
+        self._timeout = timeout
+
+
+    def set(self, key, value, timeout=None):
+        """Set key-value in cache with given timeout (or use default one)"""
+        timeout = timeout or self._timeout
+        key = self._prefix + key
+        print(key)
+        self._table.put_item(
+            Item={
+                'key': key,
+                'value': json.dumps(value),
+                'expires': int(time.time()) + timeout
+            }
+        )
+        
+
+    def get(self, key):
+        """Get key-value from cache"""
+        data = self._table.get_item(Key={ 'key': self._prefix + key })
+        print(data)
+        if data:
+            item = data['Item']
+            expires = item['expires']
+            print(expires)
+            if expires > time.time(): # type: ignore
+                print(item['value'])
+                value = item['value']
+                if isinstance(value, str):
+                    return json.loads(value)
+        return None
+        
+
+    def flush(self, pattern='', step=1000):
+        raise NotSupportedErr()
+
+        
